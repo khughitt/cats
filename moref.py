961d4ad6a99b4c11cf3226d1a03154dc6f1ab98c
Keith Hughitt <keith.hughitt@gmail.com>
2012-09-08 23:05:10 -0400

    Added initial support for highlighting start and stop codons

--- moref.py
+++ moref.py
@@ -4,16 +4,30 @@ moref:  more for FASTA files
 """
 import sys
 import os
+from argparse import ArgumentParser
 from Bio import SeqIO
+
+def get_args():
+    """Parses input and returns arguments"""
+    parser = ArgumentParser(description='Pretty-print sequence data')
+    parser.add_argument('--start-codons', dest='start_codons',
+                        action='store_true',
+                        help='Highlight any start codons in DNA/RNA output')
+    parser.add_argument('--stop-codons', dest='stop_codons',
+                        action='store_true',
+                        help='Highlight any stop codons in DNA/RNA output')
+    parser.add_argument('file', help='File containing sequence data.')
+    args = parser.parse_args()
+    return args
 
 def main():
     """moref main"""
     # Check for input file
-    if len(sys.argv) < 2:
+    args = get_args()
+    if not 'file' in args:
         print("No input file specified")
         sys.exit()
-
-    filepath = os.path.expanduser(sys.argv[1])
+    filepath = os.path.expanduser(args.file)
     
     if not os.path.isfile(filepath):
         print("No input file specified")
@@ -43,6 +57,13 @@ def main():
         # override color choices
         for k,v in config.items('dna'):
             dna[k.upper()] = "\033%s%s" % (v, k.upper())
+
+    # start/stop codons
+    stop_codons = [
+        dna['T'] + dna['A'] + dna['G'],
+        dna['T'] + dna['A'] + dna['A'],
+        dna['T'] + dna['G'] + dna['A']
+    ]
             
     # for x in range(1,10):
     # print "\033[03%dmTEST   \033[09%dmTEST" % (x,x)
@@ -52,12 +73,18 @@ def main():
     bold = '\033[1;090m'    
     
     # loop through and print seqs
+    # @NOTE - could pause here after each record if desired
     for seq in seqs:
         print(bold + seq.description)
         
         pretty = regular
         for letter in seq.seq:
             pretty += dna[letter]
+            
+        # check for stop codons if requested
+        if 'stop_codon' in args and args.stop_codon:
+            for codon in stop_codons:
+                pretty.replace(codon, '\033[0;041mSTOP') 
             
         print(pretty + "\n")
 
