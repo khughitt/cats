ba9c1382299c6991078800f860a7e105f0f497ce
Keith Hughitt <keith.hughitt@gmail.com>
2015-02-28 11:56:25 -0500

    Added tests to handle grep output

--- test/formatters/test_fasta.py
+++ test/formatters/test_fasta.py
@@ -7,16 +7,26 @@ import os
 import pytest
 
 class TestFASTAFormatter:
-    def test_format(self):
-        # base test directory
-        testdir = os.path.abspath(os.path.join( os.getcwd(), __file__, '..',  '..'))
+    @pytest.mark.parametrize('input_file,output_file', [
+        ('dna.fasta', 'dna.fasta'),
+        ('dna_grep1.fasta', 'dna_grep1.fasta'),
+        ('dna_grep2.fasta', 'dna_grep2.fasta'),
+        ('dna_grep3.fasta', 'dna_grep3.fasta')
+    ])
+
+    def test_format(self, input_file, output_file):
+        """Test FASTA formatting"""
+        testdir = os.path.abspath(os.path.join(os.getcwd(), 
+                                               __file__, '..',  '..'))
+        # input
+        infile = os.path.join(testdir, 'input', input_file)
 
         # output
         output = io.StringIO()
-        cats.format(os.path.join(testdir, 'input/dna.fasta'),
-                outbuffer=output)
+        cats.format(infile, outbuffer=output)
         output.seek(0)
 
-        with open(os.path.join(testdir, 'output/dna.fasta')) as fp:
+        with open(os.path.join(testdir, 'output', output_file)) as fp:
             expected = fp.read()
             assert str(output.read()) == expected
+
