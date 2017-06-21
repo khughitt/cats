6d22a382ca07db70116d75493717ca1ae020b7cb
Keith Hughitt <keith.hughitt@gmail.com>
2017-06-21 18:00:38 -0400

    refactored handling of shared theme elements

--- cats/styles/themes/default.py
+++ cats/styles/themes/default.py
@@ -6,6 +6,9 @@ manipulation tools. So far I have not come across any conventions for
 displaying sequence letter, otherwise that might be a reasonable default to
 use.
 """
+from cats.styles.themes import amino_acids,GREP_HIGHLIGHT_COLOR
+
+# DNA/RNA sequences
 _nucleotide_mapping = {
     "A": 28,
     "C": 12,
@@ -17,41 +20,4 @@ _nucleotide_mapping = {
 nucleotides = dict((k, '\033[38;05;%dm%s' % (v, k)) for
                         k, v in list(_nucleotide_mapping.items()))
 nucleotides['\n'] = '\n'
-
-_amino_acid_mapping = {
-    # Positively charged side chains (blue)
-    "R": 75,
-    "H": 69,
-    "K": 63,
-    # Negatively charged side chains (red)
-    "D": 168,
-    "E": 160,
-    # Polar uncharged side chains (purple)
-    "S": 189,
-    "T": 183,
-    "N": 177,
-    "Q": 171,
-    # Special cases (yellow)
-    "C": 190,
-    "U": 191,
-    "G": 192,
-    "P": 193,
-    # Hydrophobic side chains (green)
-    "A": 121,
-    "V": 120,
-    "I": 119,
-    "L": 118,
-    "M": 85,
-    "F": 84,
-    "Y": 83,
-    "W": 82,
-    "*": 255,
-    "-": 255
-}
-
-amino_acids = dict((k, '\033[38;05;%dm%s' % (v, k)) for
-                        k, v in list(_amino_acid_mapping.items()))
-amino_acids['\n'] = '\n'
-
-GREP_HIGHLIGHT_COLOR = "\033[38;05;%dm" % (226) # Yellow
 
--- cats/styles/themes/system.py
+++ cats/styles/themes/system.py
@@ -2,6 +2,7 @@
 Theme incorporating system colors
 """
 from cats.styles import colors
+from cats.styles.themes import amino_acids,GREP_HIGHLIGHT_COLOR
 
 # DNA/RNA
 nucleotides = {
@@ -14,41 +15,5 @@ nucleotides = {
     "\n": "\n"
 }
 
-_amino_acid_mapping = {
-    # Positively charged side chains (blue)
-    "R": 75,
-    "H": 69,
-    "K": 63,
-    # Negatively charged side chains (red)
-    "D": 168,
-    "E": 160,
-    # Polar uncharged side chains (purple)
-    "S": 189,
-    "T": 183,
-    "N": 177,
-    "Q": 171,
-    # Special cases (yellow)
-    "C": 190,
-    "U": 191,
-    "G": 192,
-    "P": 193,
-    # Hydrophobic side chains (green)
-    "A": 121,
-    "V": 120,
-    "I": 119,
-    "L": 118,
-    "M": 85,
-    "F": 84,
-    "Y": 83,
-    "W": 82,
-    "*": 255,
-    "-": 255
-}
-
-amino_acids = dict((k, '\033[38;05;%dm%s' % (v, k)) for
-                        k, v in list(_amino_acid_mapping.items()))
-amino_acids['\n'] = '\n'
-
-GREP_HIGHLIGHT_COLOR = "\033[38;05;%dm" % (226) # Yellow
 
 
