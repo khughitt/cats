9fd107bc25281e695c9dfc503ef1aba2aa75c8f2
Keith Hughitt <keith.hughitt@gmail.com>
2017-06-24 10:10:19 -0400

    Simplified FASTAFormetter test code

--- test/formatters/test_fasta.py
+++ test/formatters/test_fasta.py
@@ -7,29 +7,29 @@ import os
 import pytest
 
 class TestFASTAFormatter:
-    @pytest.mark.parametrize('input_file,output_file', [
-        ('dna.fasta', 'dna.fasta'),
-        ('dna_grep.fasta', 'dna_grep.fasta'),
-        ('dna.txt', 'dna.txt'),
-        ('dna_grep.txt', 'dna_grep.txt'),
-        ('dna_zgrep.txt', 'dna_zgrep.txt'),
-        ('rna.txt', 'rna.txt'),
-        ('rna_grep.txt', 'rna_grep.txt'),
-        ('protein.fasta', 'protein.fasta')
+    @pytest.mark.parametrize('filename', [
+        'dna.fasta',
+        'dna_grep.fasta',
+        'dna.txt',
+        'dna_grep.txt',
+        'dna_zgrep.txt',
+        'rna.txt',
+        'rna_grep.txt',
+        'protein.fasta'
     ])
-    def test_format(self, input_file, output_file):
+    def test_format(self, filename):
         """Test FASTA formatting"""
         testdir = os.path.abspath(os.path.join(os.getcwd(), 
                                                __file__, '..',  '..'))
         # input
-        infile = os.path.join(testdir, 'input', input_file)
+        infile = os.path.join(testdir, 'input', filename)
 
         # output
         output = io.StringIO()
         cats.format(infile, outbuffer=output, theme='default')
         output.seek(0)
 
-        with open(os.path.join(testdir, 'output', output_file)) as fp:
+        with open(os.path.join(testdir, 'output', filename)) as fp:
             expected = fp.read()
             assert str(output.read()) == expected
 
